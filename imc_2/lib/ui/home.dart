361838d55e6dac29985ef49dd672ad5a2e202164
mirnamedeiros <mirnagmedeiros@gmail.com>
2022-09-22 01:27:15 -0300

    modificação das imagens

--- imc_2/lib/ui/home.dart
+++ imc_2/lib/ui/home.dart
@@ -102,28 +102,28 @@ class _HomeState extends State<Home> {
     String _imagem = "";
 
     double peso = double.parse(pesoController.text);
-    double altura = double.parse(alturaController.text)/100;
+    double altura = double.parse(alturaController.text) / 100;
 
     double imc = peso / (altura * altura);
-    
+
     if (imc < 18.6) {
       _texto = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
       _imagem = "imagens/thin.png";
     } else if (imc >= 18.6 && imc < 24.9) {
       _texto = "Peso ideal (${imc.toStringAsPrecision(4)})";
-      _imagem = "imagens/shape.png";
+      _imagem = "imagens/inShape.png";
     } else if (imc >= 24.9 && imc < 29.9) {
       _texto = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
       _imagem = "imagens/kindaFat.png";
     } else if (imc >= 29.9 && imc < 34.9) {
       _texto = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
-      _imagem = "imagens/fat.jpg";
+      _imagem = "imagens/fat.png";
     } else if (imc >= 34.9 && imc < 39.9) {
       _texto = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
-      _imagem = "imagens/fat.jpg";
+      _imagem = "imagens/fat.png";
     } else if (imc >= 40) {
       _texto = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
-      _imagem = "imagens/fat.jpg";
+      _imagem = "imagens/fat.png";
     }
 
     Navigator.push(context,
